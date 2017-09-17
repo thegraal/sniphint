d24479eb031a10ea9a494c36f5179f89477e6f0a
thegraal <yurimaster@mail.ru>
2017-09-17 10:42:37 +0500

    Update phpbrew.sh

--- phpbrew.sh
+++ phpbrew.sh
@@ -2,10 +2,12 @@
 apt-get install libssh-dev
 phpbrew install php-5.3.28 +default+fpm +openssl=shared -- --with-openssl-dir=/usr/bin/openssl
 
-<h2>ERRORS</h2>
+#ERRORS
 checking libxml2 install dir... no
 checking for xml2-config path...
 configure: error: xml2-config not found. Please check your libxml2 installation.
 apt-get install libxml2-dev
 
+configure: error: Cannot find OpenSSL's <evp.h>
+apt-get install libcurl4-openssl-dev pkg-config
 
